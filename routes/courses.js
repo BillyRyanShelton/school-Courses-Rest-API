5481a6584efd7f2ac1b9ea349d79b640853101fb
Billy Shelton <sheltonbilly@utexas.edu>
2019-05-13 22:02:54 -0500

    added the get courses by id route.

--- routes/courses.js
+++ routes/courses.js
@@ -10,15 +10,21 @@ var sequelize = require("../models").sequelize;
 router.get('/courses', (req, res) => {
     Courses.findAll().then((courses)=>{
         res.json({
-            // for(let i =0; i < courses.length;i++){
-                // id: courses[0].userId,
-                courses,
-                // title: courses.title,
-                // description: courses.description, 
-                // estimatedTime: courses.estimatedTime,
-                // materialsNeeded: courses.materialsNeeded,
-            // }
+            courses
         });
+    });
+});
+
+router.get('/courses/:id', (req, res, next) => {
+    Courses.findByPk(req.params.id).then((course) => {
+        //if it is found then the book is updated
+        if(course) {
+            res.json({
+                course
+            });
+        } else{
+            next();
+        }
     });
 });
 
